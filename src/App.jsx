a5d1e288154fb43a6fdd43bc2197981db90fae8f
procompots <procompot64@gmail.com>
2025-10-03 14:05:00 +0500

    muhammadali

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Section2 from "./components/main/section2/Section2";
 import Section3 from "./components/main/section3/Section3";
 import Section4 from "./components/main/section4/Section4";
 import Section5 from "./components/main/section5/Section5";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 function App() {
   return (
     <>
